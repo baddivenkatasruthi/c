32cd11c134112eff66ec7decf7aaf30dc9a49018
baddivenkatasruthi <baddivenkatasruthi25@gmail.com>
2025-11-07 09:37:28 +0530

    Add files via upload

---
+++ rough.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=5;
+	int a[n]={1,2,3,4,5};
+	int t=a[n-1];
+	for(int i=n-1;i>=0;i--)
+	{
+		a[i]=a[i-1];
+	}
+	a[0]=t;
+	for(int i=0;i<n;i++) cout<<a[i];
+}
